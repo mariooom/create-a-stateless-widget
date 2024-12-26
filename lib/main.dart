e4ace5f28c24fdb88954613556c96cd7f70af905
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-12-27 00:45:25 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,   //to remove debug banner
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Colors.blueGrey,   //to change app bar background
           title: const Text(
             "My First Project",
             style: TextStyle(
@@ -18,26 +18,27 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          centerTitle: true,
+          centerTitle: true,     //put the page title in centre
         ),
-        body: Column(children: [
-          Row(
+        body: Column(     //add the widgets in column
+          children: [    
+          Row(     //to add the two images in a row widget
             children: [
-              Padding(
+              Padding(    //add padding around asset image
                 padding: const EdgeInsets.all(10.0),
-                child: Image.asset(
+                child: Image.asset(    //to add image from device
                   'assets/images/sea.jpeg',
                   width: 160,
                 ),
               ),
-              SizedBox(width: 62),
-              Image.network(
+              SizedBox(width: 62),    //add a sized box to create a gap between two images
+              Image.network(       //to add image from the internet
                 'https://picsum.photos/id/1/200/300',
                 width: 160,
               ),
             ],
           ),
-          Padding(
+          Padding(    //to add padding around the text
             padding: const EdgeInsets.all(10.0),
             child: Text(
               "The two images are displayed",
